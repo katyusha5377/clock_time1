9433ecae7af610d077c9df38437d8a3870c07e3c
katyusha5377 <yumah040825@gmail.com>
2025-01-05 00:03:07 +0900

    Update test_publisher.py

--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -1,5 +1,3 @@
-# test_publisher.py
-
 import unittest
 from clock_time.clock_publisher import get_time_message  # clock_publisherの関数をインポート
 
@@ -7,9 +5,10 @@ class TestClockPublisher(unittest.TestCase):
     def test_get_time_message(self):
         # get_time_message を呼び出し
         message = get_time_message()
-        
+
         # epoch_time が float または int であれば通過する
         self.assertTrue(isinstance(message["epoch_time"], (float, int)))
+        print(f"Tested Epoch Time: {message['epoch_time']} ({type(message['epoch_time'])})")
 
 if __name__ == "__main__":
     unittest.main()
