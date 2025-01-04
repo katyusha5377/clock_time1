ff5db621188954e32cb32ae204408caf9ddbd8a8
katyusha5377 <yumah040825@gmail.com>
2025-01-04 23:54:40 +0900

    Create clock_publisher.py

---
+++ clock_time/clock_publisher.py
@@ -0,0 +1,12 @@
+# clock_publisher.py
+
+import time
+
+def get_time_message():
+    # 現在のエポックタイム（秒単位）を取得して、必ずfloat型にする
+    epoch_time = float(time.time())
+    
+    # メッセージを返す
+    message = {"epoch_time": epoch_time}
+
+    return message
